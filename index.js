e1afba1b3f26ac4186b1cdc424141b76cc0d8a24
Nate Goldman <nate@ngoldman.me>
2015-05-31 16:08:33 -0700

    :memo: update from electron quick start

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ app.on('ready', function() {
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
+  // Open the devtools.
+  mainWindow.openDevTools();
+
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
