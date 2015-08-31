0a04ff3119900f506e9444e3f4ecf22f9d29eba4
Nate Goldman <nate@ngoldman.me>
2015-08-31 15:43:48 -0700

    :spaghetti: paste changes from quick start

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ var BrowserWindow = require('browser-window');  // Module to create native brows
 require('crash-reporter').start();
 
 // Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
+// be closed automatically when the JavaScript object is GCed.
 var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
     app.quit();
+  }
 });
 
-// This method will be called when Electron has done everything
-// initialization and ready for creating browser windows.
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
